e5b1723709fd7101b4003464793fa9b80b689973
SolomonVP <Lum7023@yandex.ru>
2024-11-02 21:18:47 +0300

    11.4 Передача параметров в бродкаст-сообщениях

--- app/src/main/java/r/d/broadcast/MyReceiver.kt
+++ app/src/main/java/r/d/broadcast/MyReceiver.kt
@@ -9,6 +9,12 @@ class MyReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
         when (intent?.action) {
+
+            "loaded" -> {
+                val percent = intent.getIntExtra("percent", 0)
+                Toast.makeText(context, "loaded $percent", Toast.LENGTH_SHORT).show()
+            }
+
             ACTION_CLICKED -> {
                 val count = intent.getIntExtra(EXTRA_COUNT, 0)
                 Toast.makeText(context, "Clicked $count", Toast.LENGTH_SHORT).show()
